eace9562d27f68ebb730cd21bcfb7395689a6727
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2020-02-03 17:42:46 +0100

    Upload full solution

---
+++ Assets/Scripts/Torch.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class Torch : MonoBehaviour
+{
+    public void SetTorch(bool enable)
+    {
+        CameraDevice.Instance.SetFlashTorchMode(enable);
+    }
+}
